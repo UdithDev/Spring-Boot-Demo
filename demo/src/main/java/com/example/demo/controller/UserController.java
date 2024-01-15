b7b880beecdcd5c8259191f82d1ae4d9cef2f825
Udith_Dev_Wee <daveudith@gmail.com>
2024-01-15 11:37:15 +0530

    create data save part

--- demo/src/main/java/com/example/demo/controller/UserController.java
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -1,15 +1,18 @@
 package com.example.demo.controller;
 
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.dto.UserDTO;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "api/v1/user")
 @CrossOrigin
 public class UserController {
+
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/getUser")
     public String getUser(){
@@ -19,5 +22,9 @@ public class UserController {
     @GetMapping
     public String getCustomers(){
         return "Customers";
+    }
+    @PostMapping("/saveUser")
+    public UserDTO saveUser(@RequestBody UserDTO userDTO){
+        return userService.saveUser(userDTO);
     }
 }
